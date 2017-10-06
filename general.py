0c3ae97bf190ac44223bed94c62388ef9dcba96e
PrestigeDox <themutantreaper@gmail.com>
2017-10-06 10:09:26 +0100

    Classes added

--- general.py
+++ general.py
@@ -1,23 +1,27 @@
 import discord
-from discord.ext.commands import Bot
+from discord.ext.commands import commands
 
-@client.command(hidden=True, aliases=['pingpong'])
-async def ping(ctx):
-	await ctx.message.delete()
-	await ctx.send("Pong!")
+class General:
+    def __init__(self, client):
+        self.client = client
 
-@client.command(hidden=True, aliases=['oke'])
-async def shrug(ctx):
-	await ctx.message.delete()
-	await ctx.send("¯\_(ツ)_/¯")
+	@commands.command(hidden=True, aliases=['pingpong'])
+	async def ping(self, ctx):
+		await ctx.message.delete()
+		await ctx.send("Pong!")
 
-@client.command(hidden=True, aliases=['spamit'])
-async def spam(ctx, count: int, *, message: str):
-	counter = 0
-	await ctx.message.delete()
-	while counter != count:
-		await ctx.send(message)
-		counter = counter + 1
+	@commands.command(hidden=True, aliases=['oke'])
+	async def shrug(self, ctx):
+		await ctx.message.delete()
+		await ctx.send("¯\_(ツ)_/¯")
+
+	@commands.command(hidden=True, aliases=['spamit'])
+	async def spam(self, ctx, count: int, *, message: str):
+		counter = 0
+		await ctx.message.delete()
+		while counter != count:
+			await ctx.send(message)
+			counter = counter + 1
 
 def setup(client):
 	client.add_cog(General(client))
